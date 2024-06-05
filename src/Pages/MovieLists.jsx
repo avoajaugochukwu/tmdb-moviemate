1184c4fbb749c37200c04a16fd5e1917f4a2c7ac
PeaceNaza <peacedonatus38@gmail.com>
2024-06-05 11:15:21 +0100

    Update movie list search input styling and position

--- src/Pages/MovieLists.jsx
+++ src/Pages/MovieLists.jsx
@@ -43,15 +43,15 @@ const MovieLists = () => {
     <main className=" min-h-screen w-full pb-10 bg-[#f5f5f5]">
       <Header />
       <div className="flex justify-center flex-col items-center ml-5">
-      <h1 className="heading-text text-black font-extrabold text-2xl mt-3">Movie Lists</h1>
+      <h1 className="text-black font-extrabold text-2xl mt-3">Movie Lists</h1>
       <div className="relative">
-        <FontAwesomeIcon icon={faSearch} className="text-[#9FA9B4] absolute top-[28%] left-[210px] h-6" />
+        <FontAwesomeIcon icon={faSearch} className="text-[#9FA9B4] absolute top-[28%] left-[200px] h-6" />
       <input type="text" placeholder="Search movies" className="outline-none rounded-xl px-5 py-1 my-3 shadow-xl" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
       </div>
         
       </div>
     
-      <div className="container grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5 mt-5 ml-3 mr-3">
+      <div className="movie-lists container grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5 mt-5 ml-3 mr-3">
       {displayTenMovies.length === 0 && <p className="text-black text-center col-span-full font-semibold text-2xl">Movie not found</p>}
         {displayTenMovies?.map((movie) => (
         <div key={movie.id} data-theme="dim" className="movie-conrtainer justify-center grid p-3 w-[250px] h-[300px] mx-3 my-3">
