1dd173a99292b908d677fdba531a70d46c3ebd89
PeaceNaza <peacedonatus38@gmail.com>
2024-06-05 12:51:03 +0100

    feat: add favorite movies  to local storage

--- src/store.js
+++ src/store.js
@@ -1,21 +1,31 @@
 import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
 
-const useStore = create((set) => ({
+const useStore = create(
+  devtools(
+    persist(
+      (set) => ({
+        favoriteMovies: [], //movies that are favorited
+      
+        toggleFavoriteMovie: (movieId) => 
+          set((state) => ({
+            favoriteMovies: state.favoriteMovies.includes(movieId)
+              ? state.favoriteMovies.filter((id) => id !== movieId)
+              : [...state.favoriteMovies, movieId],
+          })),
+          
+          searchTerm: "",
+          setSearchTerm: (term) => set({
+            searchTerm: term
+          }),
+         
+      }), {
+        name: "favorite-movies",
+        getStorage: () => localStorage,
+      
+      }
+    )
+    )
+  ); 
 
-  favoriteMovies: [], //movies that are favorited
-
-  toggleFavoriteMovie: (movieId) => 
-    set((state) => ({
-      favoriteMovies: state.favoriteMovies.includes(movieId)
-        ? state.favoriteMovies.filter((id) => id !== movieId)
-        : [...state.favoriteMovies, movieId],
-    })),
-    
-    searchTerm: "",
-    setSearchTerm: (term) => set({
-      searchTerm: term
-    })
-   
-})); 
-
-export default useStore; 
+export default useStore;
