336295af22f8467ad2c67a0b9d3519a6d8dd1b7c
PeaceNaza <peacedonatus38@gmail.com>
2024-06-11 05:24:11 +0100

    style: Update header background color

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -4,7 +4,7 @@ import "../../Styles/header.css"
 const Header = () => {
 
   return (
-    <header className='container text-secondary-100 min-w-full flex'>
+    <header className='container text-secondary-100 min-w-full flex bg-white'>
     <div className="navbar bg-base-100">
     <div className="flex-1">
     <Link to={"/"} className="btn btn-ghost text-2xl font-bold ml-9 ">MovieMate</Link>
