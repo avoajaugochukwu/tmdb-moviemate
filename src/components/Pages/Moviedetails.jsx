8bbd9ea302fa1004e6a967d0e7f4cbe03d679906
PeaceNaza <peacedonatus38@gmail.com>
2024-06-13 09:28:39 +0100

    style: Update movie details card

--- src/components/Pages/Moviedetails.jsx
+++ src/components/Pages/Moviedetails.jsx
@@ -3,7 +3,7 @@ import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 import "../../Styles/moviedetails.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome" 
-import { faHeart } from "@fortawesome/free-solid-svg-icons"
+import { faHeart, faStar } from "@fortawesome/free-solid-svg-icons"
 import useStore from "../../store"
 import "../../Styles/header.css"
 import Header from '../Layouts/Header'; 
@@ -44,31 +44,30 @@ const Moviedetails = () => {
    
     <div className='justify-center items-center flex flex-col flex-1'>
 
-    <h1 className='details-header text-center font-bold text-2xl mt-2'>{movieDetails.title}</h1>
+    <h1 className='details-header text-center font-bold text-3xl mt-2'>{movieDetails.title}</h1>
      
-     <div className='card flex-wrap text-secondary-200 bg-white lg:w-[820px] sm:w-[380px] lg:h-[340px]  mt-4 shadow-md'>
+     <div className='card flex-wrap text-secondary-200 bg-white lg:w-[900px] sm:w-[380px] lg:h-[450px]  mt-4 border'>
        {movieDetails && (
          <div className=' grid lg:md:grid-cols-2 sm:grid-cols-1 gap-2 text-sm'>
-           <div> <img src={`https://image.tmdb.org/t/p/w500/${movieDetails.poster_path}`} alt={movieDetails.title} className='w-full h-[340px] rounded' /></div>
+           <div> <img src={`https://image.tmdb.org/t/p/w500/${movieDetails.poster_path}`} alt={movieDetails.title} className='w-full h-[449px] rounded' /></div>
            <div className=" flex flex-col justify-between sm:pl-2"> 
 
-           <div className='flex justify-between gap-12 pl-2'>
-            <div><p>{movieDetails.overview}</p> </div>
+           <div className='flex justify-between gap-12 pl-2 mt-2'>
+            <div className='w-[70%]'><p className='text-[15px] leading-6 ml-3 text-secondary-500'>{movieDetails.overview}</p> </div>
            
             <div>
             <button onClick={() => toggleFavoriteMovie(movieDetails.id)}> 
-         <FontAwesomeIcon icon={faHeart} className={`heart  h-8 w-7 pr-3 
+         <FontAwesomeIcon icon={faHeart} className={`heart  h-8 w-7 mr-5 
          ${favoriteMovies.includes(movieDetails.id) ? "text-secondary-200" : "text-secondary-400" }` } />
         </button>
             </div>
            </div>
-
              <div className=' ml-2 leading-10'>
-             <p className=''>{movieDetails.genres.map((genre) => genre.name).join(", ")}</p>
+             <p className='text-sm mt-2 mb-1 ml-3 text-secondary-500'>{movieDetails.genres.map((genre) => genre.name).join(", ")}</p>
            
-           <div className='flex justify-between ml-2'>
-           <p><span className='text-secondary-300'>{movieDetails.vote_average} </span>({movieDetails.vote_count})</p>
-           <p className='mr-3'>{movieDetails.release_date}</p>
+           <div className='flex justify-between ml-3 text-xs leading-10'>
+           <p><FontAwesomeIcon icon={faStar} className="text-yellow-500 mr-1" /><span className='text-secondary-300 mr-1'>{movieDetails.vote_average} </span><span className='text-secondary-500'>({movieDetails.vote_count}+)</span></p>
+           <p className='mr-5 text-secondary-500'>{movieDetails.release_date}</p>
            </div>
            </div>
            </div>
@@ -77,7 +76,7 @@ const Moviedetails = () => {
        )} 
      </div>
     </div>
-    <div className='bg-inherit'>
+    <div className='bg-inherit mb-5'>
     <button className='back-button bg-primary text-secondary-200 hover:bg-secondary-100 hover:text-primary border border-secondary-100 shadow-inner rounded mt-8 px-7 py-1 ml-16' onClick={() => window.history.back()}>Go back</button>
     </div>
    
